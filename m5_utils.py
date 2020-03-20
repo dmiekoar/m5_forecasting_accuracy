506d3cc558543df23d008d4d9447e8b76cbdb2b6
dmiekoar <47302807+dmiekoar@users.noreply.github.com>
2020-03-20 23:01:44 +0100

    Add files via upload

---
+++ m5_utils.py
@@ -0,0 +1,45 @@
+import numpy as np 
+import pandas as pd
+import warnings
+warnings.filterwarnings('ignore')
+
+# Reduce memory usage
+def reduce_mem_usage(df, verbose=True):
+    numerics = ['int16', 'int32', 'int64', 'float16', 'float32', 'float64']
+    start_mem = df.memory_usage().sum() / 1024**2    
+    for col in df.columns:
+        col_type = df[col].dtypes
+        if col_type in numerics:
+            c_min = df[col].min()
+            c_max = df[col].max()
+            if str(col_type)[:3] == 'int':
+                if c_min > np.iinfo(np.int8).min and c_max < np.iinfo(np.int8).max:
+                    df[col] = df[col].astype(np.int8)
+                elif c_min > np.iinfo(np.int16).min and c_max < np.iinfo(np.int16).max:
+                    df[col] = df[col].astype(np.int16)
+                elif c_min > np.iinfo(np.int32).min and c_max < np.iinfo(np.int32).max:
+                    df[col] = df[col].astype(np.int32)
+                elif c_min > np.iinfo(np.int64).min and c_max < np.iinfo(np.int64).max:
+                    df[col] = df[col].astype(np.int64)  
+            else:
+                if c_min > np.finfo(np.float16).min and c_max < np.finfo(np.float16).max:
+                    df[col] = df[col].astype(np.float16)
+                elif c_min > np.finfo(np.float32).min and c_max < np.finfo(np.float32).max:
+                    df[col] = df[col].astype(np.float32)
+                else:
+                    df[col] = df[col].astype(np.float64)    
+    end_mem = df.memory_usage().sum() / 1024**2
+    if verbose: print('Mem. usage decreased to {:5.2f} Mb ({:.1f}% reduction)'.format(end_mem, 100 * (start_mem - end_mem) / start_mem))
+    return df
+
+# Show amount and percentage of missing values
+def check_missing(dataset, display = 5):
+    temp_df = dataset.copy()
+    df_nan = (temp_df.isnull().sum() / len(temp_df)) * 100
+    missing_data = pd.DataFrame({'Missing n': temp_df.isnull().sum(),'% Missing' :df_nan})
+    if missing_data['Missing n'].sum() == 0:
+        return print('Great! There are no missing values in this dataset.')
+    else:
+        return missing_data.sort_values('% Missing', ascending = False).head(display)
+
+
